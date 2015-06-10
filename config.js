83d22aa3350ce9b7ff65a45d5994cef0cba7247a
Glen Maddern <glenmaddern@gmail.com>
2015-06-10 19:26:44 +1000

    using jspm-loader-css-modules v0.1.0

--- config.js
+++ config.js
@@ -14,9 +14,9 @@ System.config({
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.5.4",
-    "babel-runtime": "npm:babel-runtime@5.5.4",
-    "core-js": "npm:core-js@0.9.14",
+    "babel": "npm:babel-core@5.5.6",
+    "babel-runtime": "npm:babel-runtime@5.5.6",
+    "core-js": "npm:core-js@0.9.15",
     "css": "npm:jspm-loader-css-modules@0.0.2",
     "path": "npm:path@0.11.14",
     "github:jspm/nodelibs-assert@0.1.0": {
@@ -52,12 +52,20 @@ System.config({
     "npm:babel-runtime@5.5.4": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
+    "npm:babel-runtime@5.5.6": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
     "npm:buffer@3.2.2": {
       "base64-js": "npm:base64-js@0.0.8",
       "ieee754": "npm:ieee754@1.1.5",
       "is-array": "npm:is-array@1.0.1"
     },
     "npm:core-js@0.9.14": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-js@0.9.15": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
