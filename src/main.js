c8694538c6176db752a5ea79b5b858bb4d760245
Glen Maddern <glenmaddern@gmail.com>
2015-06-10 19:28:08 +1000

    delete the ordering complaints

--- src/main.js
+++ src/main.js
@@ -3,9 +3,3 @@ import styles from './main.css!'
 document.querySelector('main#outlet').innerHTML = `
   <div class="${styles.lol}">JSPM and CSS Modules = &lt;3</div>
 `
-
-console.log("The order of fetching is unimportant, but the order of instantiation should have been:")
-console.log("src/a.css")
-console.log("src/backgrounds.css")
-console.log("src/a.css")
-console.log("src/main.css")
