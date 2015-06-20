46796654cbaea21d4e7f1681b3cbc5059f2549d0
Glen Maddern <glenmaddern@gmail.com>
2015-06-20 15:19:21 +1000

    bumped to the version that includes autoprefixer that works!

--- config.js
+++ config.js
@@ -18,7 +18,8 @@ System.config({
     "babel": "npm:babel-core@5.5.6",
     "babel-runtime": "npm:babel-runtime@5.5.6",
     "core-js": "npm:core-js@0.9.15",
-    "css": "npm:jspm-loader-css-modules@0.1.1",
+    "css": "npm:jspm-loader-css-modules@0.1.2",
+    "css-global": "npm:jspm-loader-css@0.1.4",
     "path": "npm:path@0.11.14",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -49,7 +50,7 @@ System.config({
     },
     "npm:buffer@3.2.2": {
       "base64-js": "npm:base64-js@0.0.8",
-      "ieee754": "npm:ieee754@1.1.5",
+      "ieee754": "npm:ieee754@1.1.6",
       "is-array": "npm:is-array@1.0.1"
     },
     "npm:core-js@0.9.15": {
@@ -57,15 +58,18 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:css-modules-loader-core@0.0.7": {
+    "npm:css-modules-loader-core@0.0.10": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "postcss": "npm:postcss@4.1.11",
-      "postcss-modules-extract-imports": "npm:postcss-modules-extract-imports@0.0.3",
-      "postcss-modules-local-by-default": "npm:postcss-modules-local-by-default@0.0.7",
-      "postcss-modules-scope": "npm:postcss-modules-scope@0.0.5"
+      "postcss-modules-extract-imports": "npm:postcss-modules-extract-imports@0.0.5",
+      "postcss-modules-local-by-default": "npm:postcss-modules-local-by-default@0.0.9",
+      "postcss-modules-scope": "npm:postcss-modules-scope@0.0.7"
     },
-    "npm:css-selector-tokenizer@0.3.1": {
+    "npm:css-selector-tokenizer@0.4.1": {
+      "fastparse": "npm:fastparse@1.1.1"
+    },
+    "npm:css-selector-tokenizer@0.5.2": {
       "fastparse": "npm:fastparse@1.1.1"
     },
     "npm:es6-promise@2.1.1": {
@@ -82,11 +86,11 @@ System.config({
     "npm:js-base64@2.1.8": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0"
     },
-    "npm:jspm-loader-css-modules@0.1.1": {
-      "jspm-loader-css": "npm:jspm-loader-css@0.1.1"
+    "npm:jspm-loader-css-modules@0.1.2": {
+      "jspm-loader-css": "npm:jspm-loader-css@0.1.4"
     },
-    "npm:jspm-loader-css@0.1.1": {
-      "css-modules-loader-core": "npm:css-modules-loader-core@0.0.7",
+    "npm:jspm-loader-css@0.1.4": {
+      "css-modules-loader-core": "npm:css-modules-loader-core@0.0.10",
       "path": "npm:path@0.11.14"
     },
     "npm:path-browserify@0.0.0": {
@@ -97,15 +101,16 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:postcss-modules-extract-imports@0.0.3": {
+    "npm:postcss-modules-extract-imports@0.0.5": {
       "postcss": "npm:postcss@4.1.11",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:postcss-modules-local-by-default@0.0.7": {
-      "css-selector-tokenizer": "npm:css-selector-tokenizer@0.3.1",
+    "npm:postcss-modules-local-by-default@0.0.9": {
+      "css-selector-tokenizer": "npm:css-selector-tokenizer@0.4.1",
       "postcss": "npm:postcss@4.1.11"
     },
-    "npm:postcss-modules-scope@0.0.5": {
+    "npm:postcss-modules-scope@0.0.7": {
+      "css-selector-tokenizer": "npm:css-selector-tokenizer@0.5.2",
       "postcss": "npm:postcss@4.1.11",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
