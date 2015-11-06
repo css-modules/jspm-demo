bd269893f864feae573ddeb2fa80d49cccfb0b9b
Glen Maddern <glenmaddern@gmail.com>
2015-11-06 17:45:41 +1000

    values are working well but live-reload isnt

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import styles from './main.css!'
-console.log(styles)
-
-import colors from 'colors.css'
-console.log(colors)
 
 document.querySelector('main#outlet').innerHTML = `
   <div class="${styles.lol}">JSPM and CSS Modules = &lt;3</div>
